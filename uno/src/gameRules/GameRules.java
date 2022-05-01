c9bf26b45666e5e4102874472854e0e08edd2bf9
Hu Hanyu <superh@Hus-MacBook-Pro.local>
2022-04-30 22:39:39 -0700

    inital setup for game rules

---
+++ uno/src/gameRules/GameRules.java
@@ -0,0 +1,54 @@
+/**
+ * Design pattern: Singleton
+ */
+
+public class GameRules {
+  // game setup relate functions
+  public boolean getPlayerNum() {
+
+  }
+
+  public void gameSetUp() {
+
+  }
+
+  // Game play relate fucntions
+  public boolean canPlayCard() {
+    // need put the player args in
+  }
+
+  public void replaceTopCard() {
+    // need put the card args in
+  }
+
+  // Player relate rules
+  public void reverse() {
+    // toggle turn
+  }
+
+  public void skip() {
+    // toggle turn
+  }
+
+  public void draw(int num) {
+    // draw nums cards
+  }
+
+  // general getter and setter
+  public void getTopCard() {
+    // return Card type
+  }
+
+  public void getPlayer(int index) {
+    // return specific player : player[index]
+  }
+
+  public void getCurrentPlayer() {
+    // return the player for current round
+  }
+
+  public void getNextPlayer() {
+    // return the next player for current round
+  }
+
+}
