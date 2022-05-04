b501c5371d6f178d001ef99be9050e9e73e3d4de
leoAshu <ashutosh.ojha2009@gmail.com>
2022-05-03 21:01:55 -0700

    modifies definition of User to inherit from Player class

--- uno/src/com/architects/uno/player/User.java
+++ uno/src/com/architects/uno/player/User.java
@@ -2,25 +2,20 @@ package com.architects.uno.player;
 
 import com.architects.uno.IDeck;
 
-public class User implements IPlayer {
-    
-    @Override
-    public void drawCard(IDeck deck, int amount) {
-        
+public class User extends Player {
+
+    public User(String playerName, IDeck deck) {
+        super(playerName, deck);
     }
     
     @Override
-    public void renderCards() {
-        
-    }
-    
-    @Override
-    public String getPlayerName() {
-        return "";
+    public void act() {
+        // perfrom action each iteration
     }
     
     @Override
     public boolean isHuman() {
         return true;
     }
+
 }
