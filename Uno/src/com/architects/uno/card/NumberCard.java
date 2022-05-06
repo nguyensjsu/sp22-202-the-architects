c88f5dc069e0e821b54fb222d8f3cc62d10634c0
Paramjot Singh <pjssunlight@gmail.com>
2022-05-06 11:13:30 -0700

    added ICard methods to NumberCard

--- Uno/src/com/architects/uno/card/NumberCard.java
+++ Uno/src/com/architects/uno/card/NumberCard.java
@@ -2,5 +2,26 @@ package com.architects.uno.card;
 
 public class NumberCard extends Card
 {
-	
+	private int value;
+	private Color color;
+
+	public boolean isSpecial()
+	{
+		return false;
+	}
+
+	public int getNumber() 
+	{
+		return value;
+	}
+
+	public Color getColor() 
+	{
+		return color;
+	}
+
+	public SpecialAction getAction()
+	{
+		return NULL_ACTION;
+	}	
 }
