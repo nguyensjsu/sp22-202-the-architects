494a29b3d92a2d14bf99a8ee55816752ab2d5192
Paramjot Singh <pjssunlight@gmail.com>
2022-05-06 16:12:12 -0700

    Added Factory Pattern

--- Uno/src/com/architects/uno/card/CardFactory.java
+++ Uno/src/com/architects/uno/card/CardFactory.java
@@ -1,5 +1,29 @@
 package com.architects.uno;
 
 public class CardFactory {
-	
+
+	public CardFactory()
+	{}
+
+	public ICard createCard(String cardType, String imageFile, Color color, int value)
+	{
+		return this.createCard(cardType, imageFile, color, value, NULL_ACTION);
+	}	
+
+	public ICard createCard(String cardType, String imageFile, Color color, SpecialAction specialAction)
+	{
+		return this.createCard(cardType, imageFile, color, -1, specialAction);
+	}		
+
+	public ICard createCard(String cardType, String imageFile, Color color, int value, SpecialAction specialAction)
+	{
+		if (cardType == "NUMBER")
+		{
+			return new NumberCard(value, imageFile, color);
+		}
+		else if (cardType == "SPECIAL")
+		{
+			return new SpecialCard(specialAction, imageFile, color);
+		}
+	}		
 }
