9c44d0743cc7bfda7738f4c4c4295be8e5a3fb1b
Paramjot Singh <pjssunlight@gmail.com>
2022-05-06 09:47:49 -0700

    Added ICard methods to SpecialCard class

--- Uno/src/com/architects/uno/card/SpecialCard.java
+++ Uno/src/com/architects/uno/card/SpecialCard.java
@@ -2,5 +2,27 @@ package com.architects.uno.card;
 
 public class SpecialCard extends Card
 {
-	
+	private SpecialAction action;
+	private Color color;
+
+	public boolean isSpecial()
+	{
+		return true;
+	}
+
+	public int getNumber() 
+	{
+		// Returns invalid number 
+		return -1;
+	}
+
+	public Color getColor() 
+	{
+		return color;
+	}
+
+	public SpecialAction getAction()
+	{
+		return action;
+	}
 }
