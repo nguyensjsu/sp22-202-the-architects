5072cbafd4102072f8dc1587b650f973acf3b328
Kureshy <78277453+Kureshy@users.noreply.github.com>
2022-05-07 07:04:48 +0500

    Update ComputersTurn.java

--- uno/src/com/architects/uno/ComputersTurn.java
+++ uno/src/com/architects/uno/ComputersTurn.java
@@ -15,13 +15,17 @@ public class ComputersTurn extends Actor  implements State
      
      public ComputersTurn(TurnState turn)
      {this.turnState=turn;
-         setImage(new GreenfootImage("Computer", size, Color.WHITE, Color.WHITE));
+       //  GreenfootImage img = new GreenfootImage("Computer", 35, Color.BLACK, new Color(0, 0, 0, 0));
+//That will make a GreenfootImage with your string with black letters on a transparent ground with a font size of 35;
+//setImage(img); //you need to set the image of your actor to the image you created to see it;
+    showTurn();
+        // setImage(new GreenfootImage("Computer", size, Color.WHITE, Color.WHITE));
         }
    
-   public void showTurn(){
-    setImage(new GreenfootImage("Computer", size, textColor, background));
+   public String showTurn(){
+    //setImage(new GreenfootImage("Computer", size, textColor, background));
    //drawImage(new GreenfootImage("Text to display", 64, null, null), 40, 40);
-
+    return "Computer";
     // getBackground().drawString("Computer", 800, 50);    
 }
     public void switchTurn(){
