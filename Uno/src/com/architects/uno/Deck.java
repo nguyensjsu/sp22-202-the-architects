eb55632ba9369937c0f277c9b921442c4f81afcc
Paramjot Singh <pjssunlight@gmail.com>
2022-05-07 13:06:33 -0700

    Added Base of Deck class

--- Uno/src/com/architects/uno/Deck.java
+++ Uno/src/com/architects/uno/Deck.java
@@ -8,9 +8,12 @@ import java.util.*;
  */
 public class Deck extends Actor implements IDeck
 {
-    private List<ICard> CardDeck = new ArrayList<ICard>();
-    public Deck() {
-        
+    private ArrayList<ICard> cardDeck = new ArrayList<ICard>();
+
+    private Deck() {}
+
+    public getInstance() {
+
     }
     /**
      * Act - do whatever the Deck wants to do. This method is called whenever
@@ -21,11 +24,18 @@ public class Deck extends Actor implements IDeck
         // Add your action code here.
     }
     
+    public void addCard(ICard card) 
+    {
+        cardDeck.add(card);
+    }
+
     public ICard drawCard() {
-        return 
+        ICard output = cardDeck.get(0);
+        cardDeck.remove(0);
+        return output;
     }
     
-    public IDeck getDeck() {
-        
+    public ArrayList<ICard> getDeck() {
+        return cardDeck;
     }
 }
