e1d9d39da3393b880c9e179a61400158ca33cf69
leoAshu <ashutosh.ojha2009@gmail.com>
2022-05-06 18:50:47 -0700

    creates two player roles implementing player role interface following strategy pattern

---
+++ uno/src/com/architects/uno/player/ComputerRole.java
@@ -0,0 +1,13 @@
+public class ComputerRole implements IPlayerRole {
+    
+    @Override
+    public void act() {
+        // act rules for computer
+    }
+
+    @Override
+    public boolean isHuman() {
+        return false;
+    }
+
+}
---
+++ uno/src/com/architects/uno/player/UserRole.java
@@ -0,0 +1,13 @@
+public class UserRole implements IPlayerRole {
+    
+    @Override
+    public void act() {
+        // act rules for user
+    }
+
+    @Override
+    public boolean isHuman() {
+        return true;
+    }
+
+}
