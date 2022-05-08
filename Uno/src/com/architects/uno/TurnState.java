a5438d51dc9651596b2eddc3d01efdd7c0ed2041
Kureshy <78277453+Kureshy@users.noreply.github.com>
2022-05-07 20:21:21 -0700

    Add files via upload

---
+++ Uno/src/com/architects/uno/TurnState.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Turn here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TurnState extends Actor
+{
+    State playersTurn;
+    State computersTurn;
+    State state = computersTurn;
+    
+    public TurnState() {
+        playersTurn = new PlayersTurn(this);
+        computersTurn = new ComputersTurn(this);
+       
+        state = computersTurn;
+        setImage(new GreenfootImage("Turn:"+state.showTurn(), 30, Color.WHITE, Color.BLUE));
+        showTurn();
+        
+       
+    }
+    
+    public void switchTurn(){
+        this.state.switchTurn();
+    }
+    
+    // public void changeTurn() {
+      //  state.turn();
+   // }
+   
+   void showTurn(){
+    state.showTurn();
+    }
+
+   void setState(State statex) {
+        this.state = statex;
+    }
+    
+     protected State getComputersTurn() {
+        return computersTurn;
+    }
+    
+    protected State getPlayersTurn() {
+        return playersTurn;
+    }
+   
+}
