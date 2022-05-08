ebfe43bd9b937442202fe6e5d0258d8c6be6a864
Kureshy <78277453+Kureshy@users.noreply.github.com>
2022-05-07 19:18:33 -0700

    Add files via upload

---
+++ game_screen_test/PlayerRules.java
@@ -0,0 +1,23 @@
+ 
+
+public class PlayerRules {
+    // Player relate rules
+    public void reverse() {
+        // toggle turn
+    }
+
+    public void skip() {
+        // toggle turn
+    }
+
+    public void draw(int num) {
+        // draw nums cards
+    }
+
+    // Game play relate fucntions
+    public boolean canPlayCard() {
+        // need put the player args in
+        return false;
+
+    }
+}
