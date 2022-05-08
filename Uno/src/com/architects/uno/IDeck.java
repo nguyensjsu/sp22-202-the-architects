06a6d153c95ea29a2c61092d5004646c5b4ff233
Kureshy <78277453+Kureshy@users.noreply.github.com>
2022-05-07 19:25:24 -0700

    Add files via upload

---
+++ Uno/src/com/architects/uno/IDeck.java
@@ -0,0 +1,7 @@
+import java.util.List;
+ 
+
+public interface IDeck {
+    public Card drawCard();
+    public List<Card> getDeck();
+}
---
+++ Uno/src/com/architects/uno/IPlayerRules.java
@@ -0,0 +1,9 @@
+public interface IPlayerRules {
+    void reverse();
+
+    void skip();
+
+    void draw(int num);
+
+    boolean canPlayCard();
+}
