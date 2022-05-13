d9ce2c2f754a1d54e21cc3447c7807a0524a0103
leoAshu <ashutosh.ojha2009@gmail.com>
2022-05-13 01:25:44 -0700

    creates a global constants class

---
+++ src/com/architects/uno/Constants.java
@@ -0,0 +1,20 @@
+/**
+ * Write a description of class Constants here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Constants  
+{
+    // screen dimensions
+    public static final int SCREEN_WIDTH = 1000;
+    public static final int SCREEN_HEIGHT = 600;
+    
+    // card dimensions
+    public static final int CARD_WIDTH = 72;
+    public static final int CARD_HEIGHT = 96;
+    
+    public static final int DECK_SIZE = 7;
+    public static final int CARD_GAP = 30;
+
+}
