bc8369ddb370c281e1d85c9e2c2cd1ed8183e4d4
Kureshy <78277453+Kureshy@users.noreply.github.com>
2022-05-13 09:03:46 +0500

    Update PlayerRules.java

--- src/com/architects/uno/PlayerRules.java
+++ src/com/architects/uno/PlayerRules.java
@@ -26,6 +26,6 @@ public class PlayerRules implements IPlayerRules
     public void draw(int num) {
        GameRules.getInstance().toggleTurn();
         GameRules.getInstance().getCurrentPlayer().drawCard(num);
-        GameRules.getInstance().toggleTurn();
+      
     }
 }
